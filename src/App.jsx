b6d8980ba6bb1044e7f81c49f773cf1a3a0ba6c8
deanna <42.dpiedra@gmail.com>
2022-10-20 14:58:06 +0300

    starting over with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,16 @@ import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
-import { AuthProvider } from "./firebase/Auth"
 
 function App() {
   return (
     //a <router> that uses the html5 history API to keep UI and URL in sync
     //everything in AuthProvider will have access to the val currentuser
     <BrowserRouter>
-      <AuthProvider>
         <Routes>
-          <Route path="/" element={<Dashboard />}></Route>
-          <Route path="/login" element={<Login />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/" element={<Login />}></Route>
         </Routes>
-      </AuthProvider>
     </BrowserRouter>
   );
 }
