a8437c87254dc3bb98c6092f303ba488c0e9dd2f
deanna <42.dpiedra@gmail.com>
2022-10-28 18:43:11 +0300

    fixed area graph

--- src/components/InternshipInsights.js
+++ src/components/InternshipInsights.js
@@ -4,12 +4,14 @@ import styled from '@emotion/styled';
 
 const InternshipInsights = () => {
     const data = [
+        {name: '' , value: ''},
         {name: 'Total Enrollments', value: 100000},
         {name: 'Completion', value: 65000},
         {name: 'Qualified Candidates', value: 850},
         {name: 'Reached Out', value: 375},
         {name: 'Interview Set', value: 300},
-        {name: 'Candidates Hired', value: 150}
+        {name: 'Candidates Hired', value: 150},
+        {name: '' , value: ''}
       ];
     return (
         <InternshipInsightsContainer>
@@ -34,8 +36,15 @@ const InternshipInsights = () => {
                 </DateBox>
                 <ResponsiveContainer width="100%"  height={174}>
                     <AreaChart data={data}>
-                        <Area type='monotone'dataKey="value" fill="#665FEF33" stroke="none" />
-                        <XAxis dataKey="name" />
+                        <Area type='monotone'dataKey="value" fill="#665FEF33" stroke="none"/>
+                        <XAxis 
+                            xAxisId={0} interval={0} tick={{fontSize: 21, fontWeight: 500}} 
+                            dataKey="value" axisLine={false} tickLine={false}
+                        />
+                        <XAxis 
+                            xAxisId={1} interval={0} tick={{fontSize: 12, fontWeight: 500}} 
+                            dataKey="name" axisLine={false} tickLine={false}
+                        />
                     </AreaChart>
                 </ResponsiveContainer>
             </DateGraphBox>
