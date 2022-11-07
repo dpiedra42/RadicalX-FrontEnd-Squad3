c5865b94fd0dca3585fd166de4efc35e5c272b54
deanna <42.dpiedra@gmail.com>
2022-11-07 18:58:41 +0200

    switched from input to textarea

--- src/components/CreateInternship/Description.jsx
+++ src/components/CreateInternship/Description.jsx
@@ -15,7 +15,7 @@ export default function Description() {
         <DescriptionContainer>
             <p>Description</p>
             <form onSubmit={(e) => handleSubmit(e)}>
-                <input 
+                <textarea
                     type="text" 
                     name="Description" 
                     value={ descriptionText }
@@ -36,4 +36,29 @@ const DescriptionContainer = styled.div`
     background-color: white;
     border-radius: 20px;
     width: 100%;  
+
+    p {
+        font-family: 'Space Grotesk';
+        font-weight: 500;
+        font-size: 24px;
+        line-height: 18px;
+        color: #333333;
+    }
+
+    textarea {
+        display: flex;
+        padding: 10px 16px;
+        width: calc(100% - 34px);
+        height: 197px;
+        background: #F1F4F8;
+        border: 1px solid #CECECE;
+        border-radius: 16px;
+        resize: none;
+
+        font-family: "Space Grotesk", sans-serif;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 24px;
+        letter-spacing: 0.3px;
+    }
 `
