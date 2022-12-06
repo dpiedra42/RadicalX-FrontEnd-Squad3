12858290b049a3f40151f15f411be398388f3fbf
deanna <42.dpiedra@gmail.com>
2022-12-06 17:01:26 -0500

    header and progress bar are now on the Surveys Page

--- src/pages/Surveys.js
+++ src/pages/Surveys.js
@@ -1,9 +1,23 @@
-// import styled from "@emotion/styled";
+import styled from "@emotion/styled";
+
+import HeaderBox from '../components/reusable/HeaderBox';
+import ProgressBar from '../components/reusable/ProgressBar';
 
 export default function Surveys() {
     return (
-        <div>
-            <h1>hello</h1>
-        </div>
+        <SurveysContainer>
+            <HeaderBox/>
+            <ProgressBar name='Surveys'/>
+        </SurveysContainer>
     )
 }
+
+const SurveysContainer = styled.div`
+    background-color: #F1F4F8;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    min-width: 1400px;
+    height: 100vh;
+    overflow: auto;
+`
