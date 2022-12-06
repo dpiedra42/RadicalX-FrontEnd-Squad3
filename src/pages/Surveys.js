8469555e51ed992c7965b0e4727f0736518f25f3
deanna <42.dpiedra@gmail.com>
2022-12-06 18:03:03 -0500

    started survey values

--- src/pages/Surveys.js
+++ src/pages/Surveys.js
@@ -1,13 +1,24 @@
 import styled from "@emotion/styled";
+// import { useState } from "react";
 
 import HeaderBox from '../components/reusable/HeaderBox';
 import ProgressBar from '../components/reusable/ProgressBar';
 
 export default function Surveys() {
+    // const [surveyValues, setSurveyValues] = useState({
+    //     Survey1: [],
+    //     Survey2: []
+    // });
+    
     return (
         <SurveysContainer>
             <HeaderBox/>
             <ProgressBar name='Surveys'/>
+            <SurveyBoxes>
+                <SurveyListNames>
+
+                </SurveyListNames>
+            </SurveyBoxes>
         </SurveysContainer>
     )
 }
@@ -20,4 +31,14 @@ const SurveysContainer = styled.div`
     min-width: 1400px;
     height: 100vh;
     overflow: auto;
+`
+const SurveyBoxes = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: calc(100% - 46px);
+    margin-top: 34px;
+    gap: 23px;
+    padding-bottom: 24px;
+`
+const SurveyListNames = styled.div`
 `
